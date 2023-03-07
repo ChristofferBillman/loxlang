24478999605a3d43c332d420ad5278706c5c7f07
ChristofferBillman <christoffer.billman@gmail.com>
2023-03-07 15:10:29 +0100

    misc: removed traceShow call.

--- app/Main.hs
+++ app/Main.hs
@@ -8,5 +8,5 @@ import Debug.Trace
 main :: IO ()
 main = do
     (arg:args) <- getArgs
-    contents   <- traceShow (arg:args) readFile arg
+    contents   <-  readFile arg
     mapM_ print $ interpret $ parse $ scanTokens contents
