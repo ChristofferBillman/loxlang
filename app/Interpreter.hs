1b4d00b338dca7929aae8a5820629140779b55c9
ChristofferBillman <christoffer.billman@gmail.com>
2023-03-07 16:25:29 +0100

    feat: implemented variable assignment

--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -14,7 +14,6 @@ import Expr
 import Stmt
 import Tokens
 import Data.Maybe
-import Debug.Trace
 
 data Environment = Environment [(String,Value)] (Maybe Environment) deriving (Show)
 
@@ -69,7 +68,7 @@ instance Fractional Value where
 (/) (NumVal n1) (NumVal n2) = NumVal (n1 Prelude./ n2)
 
 interpret :: [Stmt] -> [String]
--- TODO: Fix problem where things are interpreted the wrong way around (reverse program is quick fix).
+-- TODO: Fix problem where things are interpreted the wrong way around (reverse is quick fix).
 interpret program = reverse $ executeProgram (program, Environment [] Nothing, [])
 
 executeProgram :: ([Stmt], Environment, [String]) -> [String]
@@ -96,26 +95,37 @@ execute (stmt@(VarDeclStmt{}), env, out) = (define (stmt, env), out)
 -- Adds the variable from the declration to the environment.
 -- Returns the new environment.
 define :: (Stmt, Environment) -> Environment
+-- Case where variable is assigned a value.
 -- Confusing, but this pattern match extracts the ID from the var decl.
 define (VarDeclStmt (TOKEN _ _ (ID id) _) (Just initializer), Environment current outer) = Environment (entry : current) outer
     where
         entry = (id, init)
         (init, _) = evaluate (initializer, Environment current outer)
 
+-- Case where no value is assigned to var.
 -- Confusing, but this pattern match extracts the ID from the var decl.
 define (VarDeclStmt (TOKEN _ _ (ID id) _) Nothing, Environment current outer) = Environment (entry : current) outer
     where
         entry = (id, NilVal)
 define _ = error "Internal Error: Passed a statement that is not a variable declaration to define."
 
-get :: String -> Environment -> Value
+-- Looks a variable with a name exists, if it does it returns its value.
+-- Recursivley calls itself to search though all enclosing environments.
+get :: String -> Environment -> Maybe Value
 get key (Environment current outer)
-    | null current && isNothing outer = loxError $ "Variable " ++ key ++ " not defined"
-    | isJust value                    = fromJust value
-    | isNothing outer                 = loxError $ "Variable " ++ key ++ " not defined"
+    | null current && isNothing outer = Nothing
+    | isJust value                    = value
+    | isNothing outer                 = Nothing
     | otherwise                       = get key (fromJust outer)
         where
             value = lookup key current
+
+assign :: (String, Value) -> Environment -> Environment
+assign (name, value) (Environment current outer)
+    | isNothing result = loxError $ "Cannot assign value to undefined variable: " ++ name
+    | otherwise        = Environment ((name, fromJust result) : current) outer
+    where
+        result = get name (Environment current outer)
 
 evaluate :: (Expr, Environment) -> (Value, Environment)
 evaluate (Literal token,  env) = (toValFromLit $ getTokenLit token, env)
@@ -147,7 +157,14 @@ evaluate (Binary left opr right, env)
         (rightVal, newEnv) = evaluate (right, leftEnv)
     
 -- TODO: Call get with literal instead of str.
-evaluate (Variable (TOKEN _ str _ _), env) = (get str env,env)
+evaluate (Variable (TOKEN _ str _ _), env) 
+    | isNothing result = loxError $ "Variable " ++ str ++ " not defined"
+    | otherwise        = (fromJust result, env)
+    where
+        result = get str env
+evaluate (Assign (TOKEN _ str _ _) expr, env) = (value, assign (str, value) newEnv)
+    where
+        (value, newEnv) = evaluate (expr,env)
 evaluate _ = loxError "An unknown error occured while evaluating expression"
 
 -- Tells you whether or not a given Value is numeric or not.
